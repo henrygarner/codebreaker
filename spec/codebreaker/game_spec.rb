ec1b51d6dda725fdd0b3101a25c436e40ddb0c8e
Henry Garner <henry.garner@mac.com>
2009-10-30 11:54:26 +0000

    Codebreaker submits guess features in the green

--- spec/codebreaker/game_spec.rb
+++ spec/codebreaker/game_spec.rb
@@ -2,23 +2,43 @@ require File.join(File.dirname(__FILE__), "..", "spec_helper")
 
 module Codebreaker
   describe Game do
+    before(:each) do 
+      @messenger = mock('messenger').as_null_object
+      @game = Game.new(@messenger)
+    end
     context 'starting up' do
-      
-      before(:each) do 
-        @messenger = mock('messenger').as_null_object
-        @game = Game.new(@messenger)
-      end
-      
-      it 'should send a welcome message' do
+      it 'should send a welcome message (passing the code to start)' do
         @messenger.should_receive(:puts).with('Welcome to Codebreaker!')
-        @game.start
+        @game.start(%w[r g y c])
       end
       
       it 'should prompt for the first guess' do
         @messenger.should_receive(:puts).with('Enter guess:')
-        @game.start
+        @game.start(%w[r c g y])
       end
-      
+    end
+    context "marking a guess" do
+      context "with all 4 colors correct in the correct places" do
+        it "should mark the guess with bbbb" do
+          @game.start(%w[r g y c])
+          @messenger.should_receive(:puts).with('bbbb')
+          @game.guess(%w[r g y c])
+        end
+      end
+      context "with all 4 colors correct and 2 in the correct places" do
+        it "should mark the guess with bbww" do
+          @game.start(%w[r g y c])
+          @messenger.should_receive(:puts).with("bbww")
+          @game.guess(%w[r g c y])          
+        end
+      end
+      context "with all 4 colors correct and 1 in the correct place" do
+        it "should mark the guess with bwww" do
+          @game.start(%w[r g y c])
+          @messenger.should_receive(:puts).with('bwww')
+          @game.guess(%w[y r g c])
+        end
+      end
     end
   end
 end
